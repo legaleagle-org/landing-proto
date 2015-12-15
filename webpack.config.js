6201e3329a65f37f9254efa3f8a40b5487e04bd5
sirna <zabka.sirna.gwen@gmail.com>
2015-12-15 19:28:37 +0700

    INIT: webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path')
+,   webpack = require('webpack')
+;
+
+var webpackConfig = {
+    debug: true,
+    entry: {
+        app: path.resolve( __dirname, './src/script/core/boot.js' )
+    },
+    output: {
+        path: path.resolve( __dirname, './public/script/' ),
+        filename: 'app.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.scss$/,
+                loader: 'style!css!autoprefixer!sass'
+            }
+        ]
+    },
+    resolve: {
+        alias: {}
+    },
+    plugins: []
+};
+
+module.exports = webpackConfig;
