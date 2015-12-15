497c04d8064957f6a84aad3b24dc21449a5e27ee
sirna <zabka.sirna.gwen@gmail.com>
2015-12-15 20:09:23 +0700

    TEST: SUCCESS: gulp vendor task

--- task/tasks/watch.js
+++ task/tasks/watch.js
@@ -8,23 +8,11 @@ var gulp = require( 'gulp' )
 
 gulp.task( 'watch', [ 'setWatch' ], function() {
 
+    /* let gulp knows when markups change */
+    gulp.watch( config.webpack.input + '/**/*.html', [ 'markups' ] );
+
     /* tell webpack when client-side sources change */
-    gulp.watch( config.webpack.input + '/**/*.+(html|js|scss)', [ 'webpack' ] );
-
-    /* let gulp knows when blade markup change */
-    // gulp.watch( './client/**/*.php', [ 'markup:view' ] ); /* idk. this doesn't work */
-
-    /* let gulp knows when partial markup change */
-    gulp.watch( config.markup.cache.input, [ 'markup:cache' ] );
-
-    /* let gulp knows when stylesheet change */
-    gulp.watch( config.style.input + '/**/*.scss', [ 'styles' ] );
-
-    /* let gulp knows when dummy json change */
-    gulp.watch( './src/script' + '/**/*.dummy.json', [ 'dummy' ] );
-} );
-
-gulp.task( 'watch:style', [ 'setWatch' ], function() {
+    gulp.watch( config.webpack.input + '/**/*.js', [ 'webpack' ] );
 
     /* let gulp knows when stylesheet change */
     gulp.watch( config.style.input + '/**/*.scss', [ 'styles' ] );
