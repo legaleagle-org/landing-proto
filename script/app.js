7f34d96f6bcb088fe38d1e39a1554b7dd60d2e63
sirna <zabka.sirna.gwen@gmail.com>
2015-12-17 18:39:49 +0700

    DONE: landing section draft

--- script/app.js
+++ script/app.js
@@ -65,9 +65,21 @@
 
 	dommon.ready( function() {
 
-	    console.log( landing.viewport );
+	    var _self = this;
 
-	    this.swiper = landing.section.initSwiper();
+	    var elNav = document.getElementById( 'nav-landing' )
+	    ,   elNavLinks = elNav.querySelectorAll( '.nav-link' );
+
+	    _self.swiper = landing.section.initSwiper( elNavLinks );
+
+	    Array.prototype.forEach.call( elNavLinks, function( link, i ) {
+	        link.addEventListener( 'click', function( e ) {
+	            e.preventDefault();
+	            var swipeTargetId = e.target.dataset.slideTo || 0;
+
+	            landing.section.swipeTo( _self.swiper, swipeTargetId );
+	        })
+	    })
 	});
 
 /***/ },
@@ -156,13 +168,53 @@
 
 	var Section = {};
 
-	Section.initSwiper = function() {
-	    return new Swiper('.swiper-container', {
+	Section.currentSlide = 0;
+
+	Section.initSwiper = function( _links ) {
+
+	    var _self = this;
+
+	    var _swiper = new Swiper('.swiper-container', {
 	        nextButton: '.swiper-button-next',
 	        prevButton: '.swiper-button-prev',
 	        slidesPerView: 1,
 	        spaceBetween: 0,
-	        loop: false
+	        loop: false,
+	        hashnav: true
+	    });
+
+	    _self.currentSlide = _swiper.activeIndex;
+
+	    _self.updateLinks( _links );
+
+	    _swiper.on( 'slideChangeEnd', function () {
+	        _self.updateLinks( _links );
+	    });
+
+	    return _swiper;
+	}
+
+	Section.swipeTo = function( _swiper, _id ) {
+
+	    var _self = this;
+
+	    _id = parseInt( _id, 10 );
+
+	    if ( _self.currentSlide === _id ) return;
+
+	    _self.currentSlide = _id;
+
+	    _swiper.slideTo( _id, 300 );
+	};
+
+	Section.updateLinks = function( _links ) {
+
+	    var _self = this;
+
+	    Array.prototype.forEach.call( _links, function( _link, i ) {
+
+	        if ( parseInt( _link.dataset.slideTo, 10 ) === _self.currentSlide ) _link.classList.add( 'active' );
+	        else _link.classList.remove( 'active' );
 	    });
 	}
 
